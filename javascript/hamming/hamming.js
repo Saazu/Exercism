3023531f145116c13fe07412ea75fa202e716935
Alfred Delle <alfred.delle@yale.edu>
2021-07-17 23:34:52 +0000

    Completed Matrix, Hamming challenges

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,25 @@
+//
+// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const compute = (strand1, strand2) => {
+  if (strand1 === "") {
+    if (strand2 !== "") {
+      throw new Error("left strand must not be empty")
+    }
+  }
+
+  if (strand2 === "") {
+    if (strand1 !== "") {
+      throw new Error("right strand must not be empty")
+    }
+  }
+  if (strand1.length !== strand2.length) {
+    throw new Error('left and right strands must be of equal length');
+  }
+
+  const reducer = (totalHammingDistance, currentDNAProtein, index) => strand2[index] !== currentDNAProtein ? totalHammingDistance + 1 : totalHammingDistance;
+
+  return strand1.split("").reduce((reducer), 0)
+};
--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -3,16 +3,44 @@
 // convenience to get you started writing code faster.
 //
 
+function getRows(matrixString) {
+  return matrixString
+    .split("\n")
+    .map(str =>
+      str
+        .split(" ")
+        .map(el => Number(el)
+        )
+    );
+}
+
+function getColumns(rows) {
+  let columns = [];
+  for (let i = 0; i < rows[0].length; i++) {
+    if (columns[i] === undefined) {
+      columns[i] = [];
+    }
+    for (let j = 0; j < rows.length; j++) {
+      columns[i][j] = rows[j][i]
+    }
+  }
+  return columns
+}
+
+
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(matrixString) {
+    this._rows = getRows(matrixString)
+    this._columns = getColumns(this.rows);
   }
 
+
   get rows() {
-    throw new Error('Remove this statement and implement this function');
+    return this._rows;
   }
 
   get columns() {
-    throw new Error('Remove this statement and implement this function');
+    return this._columns;
   }
+
 }
